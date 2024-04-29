0a4e392e9d0135e4a1b0bb612eb442e3e637a8a5
abdullaahsaeed <maddaabdullah@gmail.com>
2024-04-29 22:36:11 +0500

    abdullah

---
+++ src/pages/Services/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../../components/header/index";
+
+const Services = () => {
+    return (
+        <>
+            <Header />
+            <div className="mt-24 pl-24 py-8">
+                <p className="font-ZonaNormalFont text-base">Services</p>
+                <span className="">From proof of concepts (PoC) and minimum viable products (MVP) to complex enterprise-level systems – we’ve got everything covered.</span>
+            </div>
+        </>
+    );
+};
+
+export default Services;
