fe0823652782dfff3855f6445e2dd706675e3375
Taha-tech-2003 <73897935+Taha-tech-2003@users.noreply.github.com>
2024-04-30 22:33:03 +0500

    homePage-4thsection-complete-taha

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,33 +1,51 @@
-'use client'
-import style from './header.module.css'
-import React, { useState, useEffect } from "react";
-import { useTheme } from "next-themes";
+import React, { forwardRef } from "react";
+import style from './style.module.css'
+import Magnetic from '../magnetic';
 
-
-const Header = () => {
-    const { resolvedTheme, setTheme } = useTheme()
-    const [hasMounted, setHasMounted] = useState(false);
-    useEffect(() => setHasMounted(true));
-    if (!hasMounted) return null;
+const Header = forwardRef(function index(props, ref) {
     return (
         <>
             <div className={`${style.HeaderBackground} px-8 py-8 flex items-center justify-between`}>
                 <p className="font-ZonaSemiFont text-2xl font-thin">Ronas.</p>
-                <div className="flex items-center justify-center">
-                    <a className="font-ZonaSemiFont text-1xl font-thin px-8" href="/">Services</a>
-                    <a className="font-ZonaSemiFont text-1xl font-thin px-8" href="/">Design Projects</a>
-                    <a className="font-ZonaSemiFont text-1xl font-thin px-8" href="/">Cases</a>
-                    <a className="font-ZonaSemiFont text-1xl font-thin px-8" href="/">About</a>
-                    <a className="font-ZonaSemiFont text-1xl font-thin px-8" href="/">How we work</a>
-                    <a className="font-ZonaSemiFont text-1xl font-thin px-8" href="/">Blog</a>
-                    <div onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")} className={`${style.ThemeToggle} mx-8 justify-${resolvedTheme == 'dark'?'end':'start'} bg-${resolvedTheme === 'dark'?'white':'black'}`}>
-                        <div className={`${style.ThemeToggleBtn} bg-${resolvedTheme === 'dark'?"black":"white"}`}></div> 
-                    </div>
-                    <button className="font-ZonaSemiFont text-1xl font-thin px-8 bg-white text-black dark:bg-white bg-black py-4 rounded-full px-8">Estimate Project</button>
+                <div className="flex items-center justify-content">
+                    <a className="font-ZonaSemiFont text-1xl font-thin px-8 hover:text-blue-300 transition " href="/">Services</a>
+                    <a className="font-ZonaSemiFont text-1xl font-thin px-8 hover:text-blue-300 transition  " href="/">Design Projects</a>
+                    <a className="font-ZonaSemiFont text-1xl font-thin px-8 hover:text-blue-300 transition " href="/">Cases</a>
+                    <a className="font-ZonaSemiFont text-1xl font-thin px-8 hover:text-blue-300 transition " href="/">About</a>
+                    <a className="font-ZonaSemiFont text-1xl font-thin px-8 hover:text-blue-300 transition " href="/">How we work</a>
+                    <a className="font-ZonaSemiFont text-1xl font-thin px-8 hover:text-blue-300 transition " href="/">Blog</a>
+                    <Magnetic>
+                        <div className={style.burger}>
+                            <div ref={ref} className={`${style.bounds} font-ZonaSemiFont`}>Estimate Project</div>
+                        </div>
+                    </Magnetic>
                 </div>
             </div>
         </>
     )
 }
-
+)
 export default Header;
+
+
+
+// import { forwardRef } from 'react';
+// import styles from './style.module.scss';
+
+// const Header = forwardRef(function index(props, ref) {
+//     return (
+//         <div className={styles.header}>
+//             <Magnetic>
+//                 <div className={styles.burger}>
+//                     <div ref={ref} className={styles.bounds}></div>
+//                 </div>
+//             </Magnetic>
+//         </div>
+//     )
+// }
+// )
+
+// export default Header
+
+
+//                 <button ref={ref} className="font-ZonaSemiFont text-1xl font-thin px-8 bg-white text-black py-4 rounded-full px-8">Estimate Project</button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 module.exports = {
+  darkMode:"class",
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
- 
-    // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
