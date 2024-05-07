553d9b9b3335212b32785db43c700c0907319f38
abdullaahsaeed <maddaabdullah@gmail.com>
2024-05-07 05:03:09 +0500

    abdullah

--- src/pages/Services/index.jsx
+++ src/pages/Services/index.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Header from "../../components/header/index";
 import Developmenpic from '../../../src/assets/Images/Developmenpic.jpg';
+import Nativess from '../../../src/assets/Images/Nativepic.webp'
+import CrossyPic from '../../../src/assets/Images/CrossyPic.jpg'
 import Image from 'next/image';
 import Style from "./services.module.css"
 
@@ -17,13 +19,13 @@ const Services = () => {
 
             <div className="container mx-auto">
                 <div className="md:flex mt-48">
-                    <div className="md:pl-14">
+                    <div className="md:pl-24">
                         <div className="mt-10 md:mt-24">
                             <h3 className="text-4xl font-ZonaSemiFont mb-10">Development</h3>
                             <p className="font-ZonaNormalFont text-xl">Implementing your project with scalable architecture design,modern technology, and an excellent level of data security.</p>
                         </div>
                     </div>
-                    <div className="md:w-2/3 pt-20 pr-20"> 
+                    <div className="md:w-2/3 pt-20 pr-20">
                         <Image src={Developmenpic} alt="Your Image" className={Style.Developmenpic} />
                     </div>
                 </div>
@@ -36,12 +38,46 @@ const Services = () => {
                             <span class="font-ZonaNormalFont pb-10 block">Development</span>
                             <h3 class="text-4xl font-ZonaSemiFont mb-6">Web Services</h3>
                             <p class="font-ZonaNormalFont text-sm">We create web products tailored exclusively for your business goals, be it a high converting landing page, a complex web app to automate your processes or a high-load platform with sensitive data. </p>
-                            <button class=" font-ZonaNormalFont p-4 pl-10 pr-10 mt-12 rounded-3xl border border-gray-500 rounded-md hover:bg-gray-100 hover:border-gray-600">View more</button>
+                            <button class="font-ZonaNormalFont p-4 pl-10 pr-10 mt-10 border border-gray-500 rounded-full text-white hover:bg-blue-500 hover:text-black hover:border-gray-600">View more</button>
                         </div>
                     </div>
                 </div>
             </div>
 
+            <div class="container mx-auto">
+                <div class="md:flex mt-48">
+                    <div class="md:w-1/2 px-4 md:pl-24">
+                        <div class="mt-10 md:mt-20">
+                            <span class="font-ZonaNormalFont pb-10 block">Development</span>
+                            <h3 class="text-4xl font-ZonaSemiFont mb-6">Native Mobile Development</h3>
+                            <p class="font-ZonaThinFont text-sm">We build custom mobile applications for both iOS (with Swift) and Android (with Kotlin) according to Apple and Google guidelines and can get them released to stores quickly.</p>
+                            <button class="font-ZonaNormalFont p-4 pl-10 pr-10 mt-12 border border-gray-500 rounded-full text-white hover:bg-blue-500 hover:text-black hover:border-gray-600">Android Development</button>
+                        </div>
+                        <div>
+                            <button class="font-ZonaNormalFont p-4 pl-10 pr-10 mt-5 border border-gray-500 rounded-full text-white hover:bg-blue-500 hover:text-black hover:border-gray-600">iOS Development</button>
+                        </div>
+                    </div>
+                    <div className="md:w-2/3 pt-20 pr-20">
+                        <Image src={Nativess} alt="Your Image" className={Style.Nativess} />
+                    </div>
+                </div>
+            </div>
+
+            <div class="container mx-auto pt-48">
+                <div class="md:flex mt-48">
+                    <div class="md:w-1/2 px-4 md:pl-24">
+                        <div class="mt-10 md:mt-20">
+                            <span class="font-ZonaNormalFont pb-10 block">Development</span>
+                            <h3 class="text-4xl font-ZonaSemiFont mb-6">Cross-platform Mobile Development</h3>
+                            <p class="font-ZonaThinFont text-sm">With this approach we develop a single app for multiple platforms like Android and iOS using React Native.</p>
+                            <button class="font-ZonaNormalFont p-4 pl-10 pr-10 mt-10 border border-gray-500 rounded-full text-white hover:bg-blue-500 hover:text-black hover:border-gray-600">View more</button>
+                        </div>
+                    </div>
+                    <div className="md:w-2/3 pt-20 pr-20">
+                        <Image src={CrossyPic} alt="Your Image" className={Style.CrossyPic} />
+                    </div>
+                </div>
+            </div>
 
         </>
     );
